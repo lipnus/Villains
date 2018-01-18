40b9634c4fef82080b1d1623f3c184f0d4d90ac1
m990299 <990299@naver.com>
2018-01-18 15:11:24 +0900

    login/join

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from villains import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^login/', views.signin, name='login'),
+    url(r'^join/', views.signup, name='login'),
+    
 ]
--- villains/views.py
+++ villains/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import redirect
+
+from django import forms
+
+from .forms import UserForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.template import RequestContext, loader
 
 
 def index(request):
-    return render(request, "villains/default.html", {})
+    return HttpResponse('index 페이지입니다.')
     # return render(request, "villains/default.html", {})
+
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+    else:
+        form = LoginForm()
+        return render(request, 'registration/login.html', {'form': form})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            new_user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password'],
+            )
+            return redirect('index')
+        else:   
+            form = UserForm()
+            return render(request, 'registration/adduser.html', {'form':form})
+
+    else:
+        form = UserForm()
+        return render(request, 'registration/adduser.html', {'form':form})
